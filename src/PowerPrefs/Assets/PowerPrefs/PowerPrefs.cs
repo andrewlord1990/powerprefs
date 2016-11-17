d3dae51d4f52d0b13420f3b955a3605f2b0644da
Andrew Lord <andrewlord1990@gmail.com>
2016-11-17 21:57:19 +0000

    Added documentation comments for PowerPrefs and PowerPrefsAccessor.

--- src/PowerPrefs/Assets/PowerPrefs/PowerPrefs.cs
+++ src/PowerPrefs/Assets/PowerPrefs/PowerPrefs.cs
@@ -1,4 +1,7 @@
 ﻿namespace PowerPrefs {
+  /// <summary>
+  /// Read and write values of various different types to PlayerPrefs easily.
+  /// </summary>
   public class PowerPrefs {
     
     private static PowerPrefsAccessor<bool> boolAccessor;
@@ -6,6 +9,10 @@
     private static PowerPrefsAccessor<int> intAccessor;
     private static PowerPrefsAccessor<string> stringAccessor;
 
+    /// <summary>
+    /// Create a PlayerPrefs accessor to read and write bool type values.
+    /// </summary>
+    /// <returns>Bool prefs accessor</returns>
     public static PowerPrefsAccessor<bool> ForBool() {
       if (boolAccessor == null) {
         boolAccessor = new PowerPrefsAccessor<bool>(new BoolPrefAccessor());
@@ -13,6 +20,10 @@
       return boolAccessor;
     }
 
+    /// <summary>
+    /// Create a PlayerPrefs accessor to read and write float type values.
+    /// </summary>
+    /// <returns>Float prefs accessor</returns>
     public static PowerPrefsAccessor<float> ForFloat() {
       if (floatAccessor == null) {
         floatAccessor = new PowerPrefsAccessor<float>(new FloatPrefAccessor());
@@ -20,6 +31,10 @@
       return floatAccessor;
     }
 
+    /// <summary>
+    /// Create a PlayerPrefs accessor to read and write int type values.
+    /// </summary>
+    /// <returns>Int prefs accessor</returns>
     public static PowerPrefsAccessor<int> ForInt() {
       if (intAccessor == null) {
         intAccessor = new PowerPrefsAccessor<int>(new IntPrefAccessor());
@@ -27,6 +42,10 @@
       return intAccessor;
     }
 
+    /// <summary>
+    /// Create a PlayerPrefs accessor to read and write string type values.
+    /// </summary>
+    /// <returns>String prefs accessor</returns>
     public static PowerPrefsAccessor<string> ForString() {
       if (stringAccessor == null) {
         stringAccessor = new PowerPrefsAccessor<string>(new StringPrefAccessor());
