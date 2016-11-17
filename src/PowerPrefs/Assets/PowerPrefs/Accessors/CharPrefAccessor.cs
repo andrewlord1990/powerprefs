ef3a0a888a4d14790bead02eee78dcc1c0b719bc
Andrew Lord <andrewlord1990@gmail.com>
2016-11-17 23:09:44 +0000

    Added doc comments for BoolPrefAccessor and CharPrefAccessor. Reimplemented CharPrefAccessor using an inner StringPrefAccessor.

--- src/PowerPrefs/Assets/PowerPrefs/Accessors/CharPrefAccessor.cs
+++ src/PowerPrefs/Assets/PowerPrefs/Accessors/CharPrefAccessor.cs
@@ -1,19 +1,36 @@
 namespace PowerPrefs {
 
-  using UnityEngine;
-
+  /// <summary>
+  /// Get and set char values.
+  /// </summary>
   public class CharPrefAccessor : PrefAccessor<char> {
 
+    private readonly StringPrefAccessor stringAccessor = new StringPrefAccessor();
+
+    /// <summary>
+    /// Get a char value from PlayerPrefs.
+    /// </summary>
+    /// <param name="prefKey">The key to retrieve the value for.</param>
+    /// <param name="defaultValue">
+    /// The default value to return if the key doesn't exist. If not specified it will be the default char.
+    /// </param>
+    /// <returns>The char value stored at the key prefKey or if not present then the default char.</returns>
     public char Get(string prefKey, char defaultValue = default(char)) {
-      string storedValue = PlayerPrefs.GetString(prefKey, "");
+      string storedValue = stringAccessor.Get(prefKey);
       if (storedValue.Length > 0){
         return storedValue[0];
       }
       return defaultValue;
     }
 
+    /// <summary>
+    /// Set a char value into PlayerPrefs.
+    /// </summary>
+    /// <param name="prefKey">The key to set a value for.</param>
+    /// <param name="prefValue">The value to set.</param>
+    /// <returns>This accessor.</returns>
     public PrefAccessor<char> Set(string prefKey, char prefValue) {
-      PlayerPrefs.SetString(prefKey, char.ToString(prefValue));
+      stringAccessor.Set(prefKey, prefValue.ToString());
       return this;
     }
 
