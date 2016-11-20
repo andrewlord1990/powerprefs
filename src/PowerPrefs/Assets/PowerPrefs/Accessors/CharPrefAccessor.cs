5d505872d98ecb3239bc2c22baa76d18f31192f3
Andrew Lord <andrewlord1990@gmail.com>
2016-11-20 01:30:45 +0000

    Fixed some doc comments.

--- src/PowerPrefs/Assets/PowerPrefs/Accessors/CharPrefAccessor.cs
+++ src/PowerPrefs/Assets/PowerPrefs/Accessors/CharPrefAccessor.cs
@@ -12,9 +12,9 @@ namespace PowerPrefs {
     /// </summary>
     /// <param name="prefKey">The key to retrieve the value for.</param>
     /// <param name="defaultValue">
-    /// The default value to return if the key doesn't exist. If not specified it will be the default char.
+    /// The default value to return if the key doesn't exist. If not specified it will be the built-in default
     /// </param>
-    /// <returns>The char value stored at the key prefKey or if not present then the default char.</returns>
+    /// <returns>The char value stored at the key prefKey or if not present then the built-in default.</returns>
     public char Get(string prefKey, char defaultValue = default(char)) {
       string storedValue = stringAccessor.Get(prefKey);
       if (storedValue.Length > 0){
