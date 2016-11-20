5a4a972fdec0ac6b5a59cd8eaabee954534d5c98
Andrew Lord <andrewlord1990@gmail.com>
2016-11-20 16:42:15 +0000

    Added LongPrefAccessor and tests. Fixed missing default value from CharPrefAccessor. (#12)

--- src/PowerPrefs/Assets/PowerPrefs/Accessors/LongPrefAccessor.cs
+++ src/PowerPrefs/Assets/PowerPrefs/Accessors/LongPrefAccessor.cs
@@ -1,35 +1,32 @@
 namespace PowerPrefs {
 
   /// <summary>
-  /// Get and set char values.
+  /// Get and set long values.
   /// </summary>
-  public class CharPrefAccessor : PrefAccessor<char> {
+  public class LongPrefAccessor : PrefAccessor<long> {
 
     private readonly StringPrefAccessor stringAccessor = new StringPrefAccessor();
 
     /// <summary>
-    /// Get a char value from PlayerPrefs.
+    /// Get a long value from PlayerPrefs.
     /// </summary>
     /// <param name="prefKey">The key to retrieve the value for.</param>
     /// <param name="defaultValue">
     /// The default value to return if the key doesn't exist. If not specified it will be the built-in default
     /// </param>
-    /// <returns>The char value stored at the key prefKey or if not present then the built-in default.</returns>
-    public char Get(string prefKey, char defaultValue = default(char)) {
+    /// <returns>The long value stored at the key prefKey or if not present then the built-in default.</returns>
+    public long Get(string prefKey, long defaultValue = default(long)) {
       string storedValue = stringAccessor.Get(prefKey, defaultValue.ToString());
-      if (storedValue.Length > 0){
-        return storedValue[0];
-      }
-      return defaultValue;
+      return long.Parse(storedValue);
     }
 
     /// <summary>
-    /// Set a char value into PlayerPrefs.
+    /// Set a long value into PlayerPrefs.
     /// </summary>
     /// <param name="prefKey">The key to set a value for.</param>
     /// <param name="prefValue">The value to set.</param>
     /// <returns>This accessor.</returns>
-    public PrefAccessor<char> Set(string prefKey, char prefValue) {
+    public PrefAccessor<long> Set(string prefKey, long prefValue) {
       stringAccessor.Set(prefKey, prefValue.ToString());
       return this;
     }
